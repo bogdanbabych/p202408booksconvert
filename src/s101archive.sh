8317beb47b844d16a564ed3f4bdd6c8e896b71b1
Bogdan Babych <bogdan@pc209-ital-hiwi.iued.uni-heidelberg.de>
2024-09-03 09:21:31 +0200

    s010x development

--- src/s101archive.sh
+++ src/s101archive.sh
@@ -1,2 +1,2 @@
 # python3 s101archive.py /Users/bogdan/elisp/proj/p202408books/data/BooksC
-python3 s101archive.py /Users/bogdan/elisp/proj/p202408books/data/Books
+python3 s101archive.py ../data/Books
